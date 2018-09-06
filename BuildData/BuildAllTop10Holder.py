2b6712bf7bdf404240dab896ea2c1358f45a6b90
jollyjohnny <jollygun@gmail.com>
2018-09-06 23:24:15 +0800

    add build stockholder top 10

--- BuildData/BuildAllTop10Holder.py
+++ BuildData/BuildAllTop10Holder.py
@@ -2,6 +2,7 @@
 
 import tushare as ts
 import pandas as pd
+from itertools import islice
 
 ts.set_token('d7e28c79162bc5a6fd505e5183c7d9fe45ef6fb84c2eef01b8b25bf5')
 pro = ts.pro_api()
@@ -14,7 +15,10 @@ a = pd.read_csv(stockList, encoding='utf-8')
 count = 0
 for this_ts_code in a['ts_code']:
     count = count + 1
+
     df = pro.top10_holders(ts_code=this_ts_code,start_date='20170101', end_date='20171231')
-    print(df.to_csv('../Data/stockHolder.csv'))
-    if count > 3:
-        break
+
+    if count == 1 :
+        df.to_csv('../Data/stockHolder.csv')
+    else:
+        df.to_csv('../Data/stockHolder.csv', mode='a', header=None)
