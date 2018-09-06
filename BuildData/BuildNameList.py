b45984b7e6f81740429a83b03d228cf5dab894c5
jollyjohnny <jollygun@gmail.com>
2018-09-06 21:40:00 +0800

    new folder

--- BuildData/BuildNameList.py
+++ BuildData/BuildNameList.py
@@ -1,10 +1,5 @@
 import tushare as ts
 ts.set_token('d7e28c79162bc5a6fd505e5183c7d9fe45ef6fb84c2eef01b8b25bf5')
 pro = ts.pro_api()
-
-
-
-
 data = pro.stock_basic(exchange_id='', is_hs='', fields='symbol,name,list_date,list_status')
-
 data.to_csv('./Data/stockList.csv')
